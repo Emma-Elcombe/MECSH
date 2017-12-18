6efa58e7af73de7fca00eb1fae308543d88e3476
Emma-Elcombe <e.elcombe@westernsydney.edu.au>
2017-12-19 10:07:41 +1100

    First commit, raw data to basic Home visit report for Southern NSW LHD

---
+++ sSouthernNSW/PackagesRequired.R
@@ -0,0 +1,7 @@
+#Requirements for PEIPSQ data loading and maniulations
+install.packages("readxl")
+install.packages("dplyr")
+install.packages("knitr")
+install.packages("tidyr")
+install.packages("lubridate")
+install.packages("ggplot2")
